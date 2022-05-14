47d7da45f9ff8b5d7b848325de05740a2363005d
Henrique  Medeiros <henrique.medeiros@upe.br>
2022-05-14 15:23:25 -0300

    more some atributes in  Animal class

--- PETproject/Animal.cs
+++ PETproject/Animal.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PETproject
+﻿class Animal
 {
-    internal class Animal
+    private string _species;
+    private string _name;
+    private string _identification;
+    private string _gender;
+    private string _desiredService;
+    private int _yearsOld;
+    
+
+    public void setSpecies(string Species)
     {
+        _species = Species;
+    }
+    public string getSpecies()
+    {
+        return _species;
+    }
+    public void setName(string Name)
+    {
+        _name = Name;
+    }
+    public string getName() {
+        return _name;   
+    }
+    public void setIdentification(string Identification) {
+        _identification = Identification;
+    }
+    public string getIdentification() {
+        return _identification;
+    }
+    public void setGender(string Gender) {
+        _gender =Gender;   
+    }
+    public string getGender() {
+        return _gender;
+    }
+    public void setDesiredService(string DesiredService) {
+        _desiredService = DesiredService;
+    }
+    public string getDesiredService() {
+        return _desiredService;
+    }
+    public void setYearsOld(int YearsOld) {
+        _yearsOld = YearsOld;
+    }
+    public int getYearsOld() {
+        return _yearsOld;
     }
 }
+
+
+
